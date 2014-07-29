3c926f9176a82f879a902995924d402e11cc29eb
Aayushi Kaushik <akaushik@uwaterloo.ca>
2014-07-29 13:38:45 -0400

    Finished hardcoding test input

--- KinectSimpleGesture/KinectSimpleGesture/GestureDetect.cs
+++ KinectSimpleGesture/KinectSimpleGesture/GestureDetect.cs
@@ -22,6 +22,7 @@ namespace KinectSimpleGesture
             Dictionary<JointType, JointData> dict2 = new Dictionary<JointType, JointData>();
             Dictionary<JointType, JointData> dict3 = new Dictionary<JointType, JointData>();
             Dictionary<JointType, JointData> dict4 = new Dictionary<JointType, JointData>();
+			Dictionary<JointType, JointData> dict5 = new Dictionary<JointType, JointData>();
             
             // Hard code test data into the trie
             JointData data = new JointData(JointType.ElbowLeft, -13.8886118490198, 2.38573102788801, 10);
@@ -41,7 +42,7 @@ namespace KinectSimpleGesture
             data = new JointData(JointType.WristRight, 1.37402396087154, -79.134973625391, 10);
             dict1.Add(JointType.WristRight, data);
             GestureSegment segment = new GestureSegment(dict1);
-            TrieNode trie = new TrieNode(segment);
+			TrieNode trie = new TrieNode(segment); // parent
 
             data = new JointData(JointType.ElbowLeft, -13.9888844123371, 2.21115891220704, 10);
             dict2.Add(JointType.ElbowLeft, data);
@@ -61,39 +62,63 @@ namespace KinectSimpleGesture
             dict2.Add(JointType.WristRight, data);
             segment = new GestureSegment(dict2);
             TrieNode child = trie.addChild(segment);
-            int i = 0;
             
-            /*
-             * Update here UpdateGesture Segment: 
-                ElbowLeft -13.9836244839633 1.71891913438868
-                ElbowRight 4.3049878323027 43.4621989227762
-                HandLeft -14.7151801589239 33.2339854987523
-                HandRight 3.46824467371025 76.7099660420278
-                ShoulderLeft -11.1845941305872 -34.1881489323549
-                ShoulderRight -1.38314797157102 -80.412653664972
-                WristLeft -14.632609630084 26.9411859292547
-                WristRight 3.98281998920187 70.7508683900332
-             * 
-             * Update here UpdateGesture Segment: 
-                ElbowLeft -14.0028558580185 1.79803376615876
-                ElbowRight 4.90968971991051 35.588217052244
-                HandLeft -14.7223904525819 33.2593668874657
-                HandRight 13.1459056125209 37.7609138521562
-                ShoulderLeft -11.3454599877182 -33.7682599948257
-                ShoulderRight -1.51655913531167 -79.4708437301563
-                WristLeft -14.6397518392029 26.9798967256435
-                WristRight 11.1063083347709 37.2859755350544
-             * 
-             * Update here UpdateGesture Segment: 
-                ElbowLeft -13.9022953696224 1.35316570984196
-                ElbowRight 3.95160036770324 49.6285706518732
-                HandLeft -14.682648049446 32.8898131784065
-                HandRight -1.6898189459153 -82.7667453028595
-                ShoulderLeft -11.0641066611262 -34.0569025127242
-                ShoulderRight -0.901948551797491 -84.4164358488472
-                WristLeft -14.6108300566225 26.5385944815128
-                WristRight 0.0893234273827823 89.5477381361585
-             */
+            data = new JointData(JointType.ElbowLeft, -13.9836244839633, 1.71891913438868, 10);
+			dict3.Add(JointType.ElbowLeft, data);
+			data = new JointData(JointType.ElbowRight, 4.3049878323027, 43.4621989227762, 10);
+			dict3.Add(JointType.ElbowRight, data);
+			data = new JointData(JointType.HandLeft, -14.7151801589239, 33.2339854987523, 10);
+			dict3.Add(JointType.HandLeft, data);
+			data = new JointData(JointType.HandRight, 3.46824467371025, 76.7099660420278, 10);
+			dict3.Add(JointType.HandRight, data);
+			data = new JointData(JointType.ShoulderLeft, -11.1845941305872, -34.1881489323549, 10);
+			dict3.Add(JointType.ShoulderLeft, data);
+			data = new JointData(JointType.ShoulderRight, -1.38314797157102, -80.412653664972, 10);
+			dict3.Add(JointType.ShoulderRight, data);
+			data = new JointData(JointType.WristLeft, -14.632609630084, 26.9411859292547, 10);
+			dict3.Add(JointType.WristLeft, data);
+			data = new JointData(JointType.WristRight, 3.98281998920187, 70.7508683900332, 10);
+			dict3.Add(JointType.WristRight, data);
+			segment = new GestureSegment(dict3);
+			TrieNode child2 = child.addChild(segment);
+
+			data = new JointData(JointType.ElbowLeft, -14.0028558580185, 1.79803376615876, 10);
+			dict4.Add(JointType.ElbowLeft, data);
+			data = new JointData(JointType.ElbowRight, 4.90968971991051, 35.588217052244, 10);
+			dict4.Add(JointType.ElbowRight, data);
+			data = new JointData(JointType.HandLeft, -14.7223904525819, 33.2593668874657, 10);
+			dict4.Add(JointType.HandLeft, data);
+			data = new JointData(JointType.HandRight, 13.1459056125209, 37.7609138521562, 10);
+			dict4.Add(JointType.HandRight, data);
+			data = new JointData(JointType.ShoulderLeft, -11.3454599877182, -33.7682599948257, 10);
+			dict4.Add(JointType.ShoulderLeft, data);
+			data = new JointData(JointType.ShoulderRight, -1.51655913531167, -79.4708437301563, 10);
+			dict4.Add(JointType.ShoulderRight, data);
+			data = new JointData(JointType.WristLeft, -14.6397518392029, 26.9798967256435, 10);
+			dict4.Add(JointType.WristLeft, data);
+			data = new JointData(JointType.WristRight, 11.1063083347709, 37.2859755350544, 10);
+			dict4.Add(JointType.WristRight, data);
+			segment = new GestureSegment(dict4);
+			TrieNode child3 = child2.addChild(segment);
+
+            data = new JointData(JointType.ElbowLeft, -13.9022953696224, 1.35316570984196, 10);
+			dict5.Add(JointType.ElbowLeft, data);
+			data = new JointData(JointType.ElbowRight, 3.95160036770324, 49.6285706518732, 10);
+			dict5.Add(JointType.ElbowRight, data);
+			data = new JointData(JointType.HandLeft, -14.682648049446, 32.8898131784065, 10);
+			dict5.Add(JointType.HandLeft, data);
+			data = new JointData(JointType.HandRight, -1.6898189459153, -82.7667453028595, 10);
+			dict5.Add(JointType.HandRight, data);
+			data = new JointData(JointType.ShoulderLeft, -11.0641066611262, -34.0569025127242, 10);
+			dict5.Add(JointType.ShoulderLeft, data);
+			data = new JointData(JointType.ShoulderRight, -0.901948551797491, -84.4164358488472, 10);
+			dict5.Add(JointType.ShoulderRight, data);
+			data = new JointData(JointType.WristLeft, -14.6108300566225, 26.5385944815128, 10);
+			dict5.Add(JointType.WristLeft, data);
+			data = new JointData(JointType.WristRight, 0.0893234273827823, 89.5477381361585, 10);
+			dict5.Add(JointType.WristRight, data);
+			segment = new GestureSegment(dict5);
+			TrieNode child4 = child3.addChild(segment);
 
         }
 
